8b540753f7edd5e954047e0b402c0f1a036a32d7
vincent heloin <vincent.heloin@gmail.com>
2026-01-27 04:34:59 +0000

    The calendar rendering is breaking the 'Week' rows. It looks like each w

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -32,11 +32,11 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse",
-        head_row: "flex",
+        head_row: "grid grid-cols-7",
         head_cell:
-          "text-muted-foreground rounded-md w-8 font-normal text-center text-[0.8rem]",
-        row: "flex w-full mt-1",
-        cell: "h-8 w-8 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+          "text-muted-foreground rounded-md font-normal text-center text-[0.8rem]",
+        row: "grid grid-cols-7 w-full mt-1",
+        cell: "h-8 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-8 w-8 p-0 font-normal aria-selected:opacity-100"
