11c20b61fc67657886ee51aa9e9af6190834cfa4
vincent heloin <vincent.heloin@gmail.com>
2026-01-27 16:17:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8547100048-ba99d",
+  "appId": "1:458707956556:web:2dd7122c3e66cf9d0895e9",
+  "apiKey": "AIzaSyBgI_uU5c4NEVqRA2s5QcstF8P1wbsB4VY",
+  "authDomain": "studio-8547100048-ba99d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "458707956556"
+};
