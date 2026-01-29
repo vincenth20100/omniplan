d2641ff69c7d8e44f953273ec49203351d0b5a85
vincent heloin <vincent.heloin@gmail.com>
2026-01-29 05:55:52 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialog is not defin

--- src/components/project-settings-dialog.tsx
+++ src/components/project-settings-dialog.tsx
@@ -8,6 +8,16 @@ import {
   DialogFooter,
   DialogClose,
 } from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
